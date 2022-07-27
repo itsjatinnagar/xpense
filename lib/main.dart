9417b0c342053ad551f24872abed4c62e017e4a8
Jatin Nagar <itsjatinnagar@gmail.com>
2022-07-27 16:03:53 +0530

    Override linting rules

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Xpense',
       home: Scaffold(),
     );
