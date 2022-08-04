cac3453d0564f73f71b1dc1be79254aeec4e63c4
Jatin Nagar <itsjatinnagar@gmail.com>
2022-08-04 19:24:57 +0530

    Moved: Logic from UI to Model

--- lib/models/transaction_manager.dart
+++ lib/models/transaction_manager.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:xpense/databases/expense_db.dart';
 
 import 'transaction.dart';
@@ -8,7 +8,27 @@ class TransactionManager extends ChangeNotifier {
 
   final List<Expense> _list;
 
-  List<Expense> get transactions => List.unmodifiable(_list);
+  List<Map<String, dynamic>> get groupedTransactions {
+    final DateTime registeredOn = DateTime(2022, 8, 1);
+    final DateTime today =
+        DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+    final temp = DateTimeRange(start: registeredOn, end: today).duration.inDays;
+    final int length = temp < 7 ? 7 : temp;
+
+    final List<Map<String, dynamic>> list = [];
+
+    for (var i = 0; i <= length; i++) {
+      final date = today.subtract(Duration(days: i));
+      final dayExpenses = _list
+          .where((element) => date.compareTo(element.dateTime) == 0)
+          .toList();
+      double daySpending = dayExpenses.fold(
+          0.0, (previousValue, element) => previousValue + element.amount);
+      list.add(
+          {'date': date, 'spending': daySpending, 'expenses': dayExpenses});
+    }
+    return list;
+  }
 
   void addTransaction(Expense transaction) async {
     transaction = await ExpenseDatabaseManager.instance.insert(transaction);
